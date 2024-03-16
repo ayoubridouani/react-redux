07fcf737eb56fcbe0fc368045a1da8a825f65306
Ayoub Ridouani <ayoub.ridouani@usmba.ac.ma>
2024-03-16 00:50:08 +0000

    redux with redux devtools extension

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,8 @@
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import { rootReducer } from './rootReducer'
 import logger from 'redux-logger'
 
-export const store = createStore(rootReducer, applyMiddleware(logger))
+//export const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+export const store = createStore(rootReducer, /* preloadedState, */ composeEnhancers(applyMiddleware(logger)));
